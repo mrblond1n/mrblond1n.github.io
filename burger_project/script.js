82fa7282a6c614d2c25dfd7721fe096fbbef444d
MrBlond1n <nikita.kirdiapin@gmail.com>
2019-05-25 12:19:27 +0300

    u

--- burger_project/script.js
+++ burger_project/script.js
@@ -1,6 +1,6 @@
 let menuBtn = document.body.querySelector('.menu-hamburger'), 
 menuBtnClose = document.body.querySelector('.close-btn'), 
-menuList = document.body.querySelector('.menu-fscr');
+menuList = document.body.querySelector('.menu-fullscreen');
 
 menuBtn.addEventListener('click', function() {
   menuList.classList.remove('deactive');
