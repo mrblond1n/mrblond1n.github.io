8564e9649746cbb2a12e8a68142a4bc55cbc26f4
MrBlond1n <nikita.kirdiapin@gmail.com>
2019-06-01 16:29:54 +0300

    u

--- burger_project/form.js
+++ burger_project/form.js
@@ -4,7 +4,10 @@ const form = document.querySelector('#form-order'),
   successOrder = document.querySelector('.success-order'),
   failedOrder = document.querySelector('.failed-order'),
   closeSuccessMessage = document.querySelector('#close__success-message'),
-  closeFailedMessage = document.querySelector('#close__failed-message');
+  closeFailedMessage = document.querySelector('#close__failed-message'),
+  requiredItems = document.querySelectorAll('.required'),
+  numberOnly = document.querySelectorAll('.number-only'),
+  textOnly = document.querySelector('.text-only');
 
 function reqReadyStateChange() { // ??
   if (xhr.readyState == 4 && xhr.status == 200) {
@@ -18,10 +21,19 @@ function reqReadyStateChange() { // ??
       closeSuccessMessage.addEventListener('click', () => {
         successOrder.style.display = "none";
         document.body.classList.remove('lock');
+        for (let i = 0; i < requiredItems.length; i++) {
+          requiredItems[i].classList.remove('required--active');
+        };
         document.querySelector('#resetButton').click();
       });
     } else if (message.status == 0) {
       console.log('Ошибка в принятии заказа, попробуйте позже');
+      for (let i = 0; i < requiredItems.length; i++) {
+        requiredItems[i].classList.add('required--active');
+        requiredItems[i].addEventListener('click', () => {
+          requiredItems[i].classList.remove('required--active');
+        })
+      }
       failedOrder.style.display = "flex";
       document.body.classList.add('lock');
       closeFailedMessage.addEventListener('click', () => {
@@ -65,4 +77,20 @@ orderButton.addEventListener('click', function (e) {
   e.preventDefault();
 
   ajaxForm(form);
+});
+
+for (let i = 0; i < numberOnly.length; i++) {
+  numberOnly[i].addEventListener('keydown', function (e) {
+
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+      e.preventDefault();
+    };
+
+  });
+};
+
+textOnly.addEventListener('keydown', function (e) {
+  if (isFinite(e.key) || e.key == '+' || e.key == '/') {
+    e.preventDefault();
+  };
 });
